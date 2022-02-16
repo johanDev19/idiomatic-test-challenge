9c78bce1d11ef4742053256a4a4c67a9b448b6d3
johan guzman <johan.dev19@gmail.com>
2022-02-16 09:28:03 -0400

    refactor: add the html structure

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,8 +1,35 @@
 import Card from "../common/Card/Card";
+import Input from "../common/Input";
+import Label from "../common/Label/Label";
 import styles from "./form.module.css";
 
 function Form() {
-  return <Card className={styles.formContainer}>form go here</Card>;
+  return (
+    <Card className={styles.formContainer}>
+      <div>
+        <Label>Filter prefix:</Label>
+        <Input />
+      </div>
+      <div>
+        <textarea />
+        <div>
+          <div>
+            <Label>Name:</Label>
+            <Input />
+          </div>
+          <div>
+            <Label>Surname:</Label>
+            <Input />
+          </div>
+        </div>
+      </div>
+      <div>
+        <button>Create</button>
+        <button>Update</button>
+        <button>Delete</button>
+      </div>
+    </Card>
+  );
 }
 
 export default Form;
