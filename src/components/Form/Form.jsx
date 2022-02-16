38e438c59408ed9b6d664b96a22172df92feb4b6
johan guzman <johan.dev19@gmail.com>
2022-02-16 17:55:16 -0400

    feat: disable button if no name was entered

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import classNames from "classnames";
+
 import Button from "../common/Button";
 import Card from "../common/Card";
 import Input from "../common/Input";
@@ -6,7 +8,35 @@ import Label from "../common/Label";
 import Select from "../common/Select";
 import styles from "./form.module.css";
 
+const NAME = "name";
+const SURNAME = "surname";
+
 function Form() {
+  const [canUpdate, setCanUpdate] = useState(false);
+  const [canDelete, setCanDelete] = useState(false);
+  const [canCreate, setCanCreate] = useState(false);
+  const [name, setName] = useState("");
+  const [surname, setSurName] = useState("");
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    if (name === NAME) {
+      setName(value);
+
+      if (value.length > 0) {
+        setCanUpdate(true);
+        setCanDelete(true);
+        setCanCreate(true);
+      } else {
+        setCanUpdate(false);
+        setCanDelete(false);
+        setCanCreate(false);
+      }
+    } else if (name === SURNAME) {
+      setSurName(value);
+    }
+  };
+
   return (
     <Card className={classNames(styles.formContainer, "shadow-md")}>
       <div className="flex grow">
@@ -23,18 +53,34 @@ function Form() {
         <div className="flex flex-col items-end">
           <div className="flex justify-between w-full">
             <Label className={styles.label}>Name:</Label>
-            <Input className="w-full" />
+            <Input
+              className="w-full"
+              name={NAME}
+              value={name}
+              onChange={handleInputChange}
+            />
           </div>
           <div className="flex mt-2 w-full justify-between">
             <Label className={classNames(styles.label)}>Surname:</Label>
-            <Input className="w-full" />
+            <Input
+              className="w-full"
+              name={SURNAME}
+              value={surname}
+              onChange={handleInputChange}
+            />
           </div>
         </div>
       </div>
       <div className="flex justify-start gap-4 mt-4">
-        <Button color="#6ee7b7">Create</Button>
-        <Button color="#fce689">Update</Button>
-        <Button color="#f9a6a5">Delete</Button>
+        <Button disabled={!canCreate} color="#6ee7b7">
+          Create
+        </Button>
+        <Button disabled={!canUpdate} color="#fce689">
+          Update
+        </Button>
+        <Button disabled={!canDelete} color="#f9a6a5">
+          Delete
+        </Button>
       </div>
     </Card>
   );
--- src/components/common/Button/Button.jsx
+++ src/components/common/Button/Button.jsx
@@ -2,11 +2,13 @@ import classNames from "classnames";
 
 import styles from "./button.module.css";
 
-function Button({ children, className, color }) {
+function Button({ children, className, color, disabled, onClick }) {
   return (
     <button
       className={classNames(styles.button, className, "shadow-md")}
       style={{ backgroundColor: color }}
+      disabled={disabled}
+      onClick={onClick}
     >
       {children}
     </button>
