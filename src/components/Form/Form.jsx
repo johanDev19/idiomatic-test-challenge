08ccc93fbf8174ec2dd12348b3b9772660e27596
johan guzman <johan.dev19@gmail.com>
2022-02-15 23:59:16 -0400

    feat: create the form component

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,8 @@
+import Card from "../common/Card/Card";
+import styles from "./form.module.css";
+
+function Form() {
+  return <Card className={styles.formContainer}>form go here</Card>;
+}
+
+export default Form;
