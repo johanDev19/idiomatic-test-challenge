1c1c7cb41e73ab50d5a3e696c22d35f0863e68be
johan guzman <johan.dev19@gmail.com>
2022-02-15 23:59:32 -0400

    feat: create the home component

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import Container from "../../components/common/Container";
+import Form from "../../components/Form";
+
+function Home() {
+  return (
+    <Container>
+      <Form />
+    </Container>
+  );
+}
+
+export default Home;
